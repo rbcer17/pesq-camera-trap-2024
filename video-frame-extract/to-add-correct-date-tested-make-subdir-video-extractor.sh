ee6505963396b2e3c223a8cfafc263eb5669e032
Roberto Cavalcanti <63593378+rbcer17@users.noreply.github.com>
2024-06-03 15:50:26 -0300

    Add files via upload

---
+++ video-frame-extract/to-add-correct-date-tested-make-subdir-video-extractor.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright (c) 2020, lowkey digital studio
+# Author: Nathan Wolek
+# Usage of this file and its contents is governed by the MIT License
+# Modified by Roberto Cavalcanti to extract frames from video
+#
+# to run use syntax:  ./make-subdir-video-extractor.sh *.AVI
+#
+# BEGUN - 16 April 2020
+# GOAL - generate spectrogram image of entire .WAV file
+# expected input .wav files (upper or lower case extension) produced by the AudioMoth
+# expected output .png files visualizing entire .WAV file
+#make new directory for frames
+mkdir frames
+# iterate through all arguments
+for file in $@
+do
+
+	# strip out the filename without path
+	without_path="${file##*/}"
+	
+	# use conditionals to make wav extension case insensitive
+	if [[ $without_path == *.avi ]] || [[ $without_path == *.AVI ]]; then
+	
+		# strip the extension
+		without_extension="./frames/${without_path%.*}"
+	
+
+		# generate the initial .png spectrogram output from sox
+		# dimension here are for spectrogram only, extra padding will result in 1280 x 720 image
+		echo "making frame extraction for $without_path..."
+
+                ffmpeg -i "$file" -vf fps=1/1 "$without_extension"%04d.jpg
+
+	
+	else
+		echo "skipped $without_path - not an avi file!"
+	fi
+	
+done
